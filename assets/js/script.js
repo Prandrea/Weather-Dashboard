68045356af8badbb81c1e484b3fcf2f2ef171d4a
prandrea <prandrea@utexas.edu>
2022-06-06 16:41:47 -0500

    new function weather search

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,66 +10,13 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
-const API_KEY = "fc552d2e0473bcbae68aa48291540746"
-
-const timeEl = document.getElementById('time');
-const dateEl = document.getElementById('date');
-const cityEl = document.getElementById('city');
-const stateEl = document.getElementById('state');
-const currentWeathcondEl = document.getElementById('weather-conditions');
-const ForecastEl = document.getElementById('forecast');
-const currentTempEl = document.getElementById('current-temperature');
 
 
-
-function showWeatherData (data){
-    let {temp,humidity, wind_speed, uv_index} = data.current;
-
-    currentWeatherItemsEl.innerHTML =
-    `<div class="weather-item">
-        <div>Temperature</div>
-        <div>${temp}</div>
-    </div>
-    <div class="weather-item">
-        <div>Humidity</div>
-        <div>${humidity}</div>
-    </div>
-
-    <div class="weather-item">
-        <div>Wind Speed</div>
-        <div>${wind_speed}</div>
-    </div>
-
-    <div class="weather-item">
-        <div>UV Index</div>
-        <div>${uv_index}</div>
-    </div>`
-
-    ;
-
-    let fiveDayForecast= ''
-    data.daily.forEach((day, idx) => {
-        if(idx == 0){
-            currentTempEl.innerHTML = `
-                <img src=" http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-                <div class="weather-forecast-item">
-                <div class="day">${window.moment(day.dt*1000).format('ddd')}</div><div class="temp">${day.temp.day}&#176; F</div>
-            </div>
-            `
-
-        }else{
-            fiveDayForecast += `
-            <div class="weather-forecast-item">
-            <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
-            <img src=" http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-            <div class="temp">${day.temp.day}&#176; F</div>
-            </div>
-            `
+var weather = function () {
+    var apiKey = 'fc552d2e0473bcbae68aa48291540746'
+    var city = document.getElementById('citysearch').value
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`)
+        .then(res => res.json())
 
 
-        }
-        
-    })
-
-    weatherForecastEl.innerHTML = fiveDayForecast;
 }
